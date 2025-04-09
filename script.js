952712f0cab0c1314c569290e595e196242ebcc5
Matheus Mariano Euzébio <101993538+ItsUnder@users.noreply.github.com>
2025-04-09 17:00:57 -0300

    Added Original Files
    The upload of the first finished work of the website prototype

---
+++ script.js
@@ -0,0 +1,42 @@
+let darkMode = localStorage.getItem("dark-mode");
+console.log("hello");
+
+window.onload = function themeChecker() {
+  if (darkMode === "enabled") {
+    changeThemes();
+    localStorage.setItem("dark-mode", "enabled");
+  }
+}
+
+function changeThemes() {
+    darkMode = localStorage.getItem("dark-mode");
+    if (darkMode === "disabled") {
+      document.body.classList.toggle("dark");
+      localStorage.setItem("dark-mode", "enabled");
+    } else {
+      document.body.classList.toggle("dark");
+      localStorage.setItem("dark-mode", "disabled");
+    }
+}
+function splashScreenChecker() {
+  if(!localStorage.getItem("splash-shown")) {
+    splashScreen(); 
+    console.log("WE")
+  } else {  
+    document.getElementById("splash").style.opacity = "0";
+    document.getElementById("splash").style.display = "none";
+    document.getElementById("content").classList.add("show-content");
+  }
+}
+splashScreenChecker();
+
+function splashScreen() {
+setTimeout(() => {
+  document.getElementById("splash").style.opacity = "0";
+  localStorage.setItem("splash-shown", "yes");
+  setTimeout(() => {
+   document.getElementById("content").classList.add("show-content");
+   document.getElementById("splash").style.display = "none";
+  }, 1000);
+ }, 2000);
+}
